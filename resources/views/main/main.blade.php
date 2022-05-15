9f6112783e7107449c72b0a794ddc5bbbbe5ffd3
Mateusz Krysiak <monti02022@gmail.com>
2022-05-15 13:34:29 +0200

    [UPDATE] CSS

--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -166,4 +166,79 @@
 
 </div>
 
+<hr class="breakline">
+
+<div class="portfolio">
+    <h4 class="our-portfolio">NASZE PORTFOLIO</h4>
+    <h4>FOTOGRAFIA</h4>
+    <h3>PORTFOLIO</h3>    
+    <div class="row p-0 m-0">
+        <div class="col-lg-6 p-0 m-0">
+            <img src="{{ url('/images/img/mateusz.jpg') }}" alt="" class="img-fluid">
+        </div>
+        <div class="col-lg-6 p-0 m-0">
+            <img src="{{ url('/images/img/mateusz.jpg') }}" alt="" class="img-fluid">
+        </div>
+    </div>
+    <div class="row p-0 m-0">
+        <div class="col-lg-6 p-0 m-0">
+            <img src="{{ url('/images/img/mateusz.jpg') }}" alt="" class="img-fluid">
+        </div>
+        <div class="col-lg-6 p-0 m-0">
+            <img src="{{ url('/images/img/mateusz.jpg') }}" alt="" class="img-fluid">
+        </div>
+    </div>
+</div>
+
+<hr class="breakline">
+
+<div class="portfolio mt-5">
+    <h4>FILMOGRAFIA</h4>
+    <h3>PORTFOLIO</h3>    
+    <div class="container">
+        <div class="row my-4">
+            <div class="col-12 movies">
+                <div class="youtube-player" data-id="R9vtT7CEETo"></div>
+                <div class="text">BACKSTAGE</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    /*
+     * Light YouTube Embeds by @labnol
+     * Credit: https://www.labnol.org/
+     */
+  
+    function labnolIframe(div) {
+      var iframe = document.createElement('iframe');
+      iframe.setAttribute('src', 'https://www.youtube.com/embed/' + div.dataset.id + '?autoplay=1&rel=0');
+      iframe.setAttribute('frameborder', '0');
+      iframe.setAttribute('allowfullscreen', '1');
+      iframe.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture');
+      div.parentNode.replaceChild(iframe, div);
+    }
+  
+    function initYouTubeVideos() {
+      var playerElements = document.getElementsByClassName('youtube-player');
+      for (var n = 0; n < playerElements.length; n++) {
+        var videoId = playerElements[n].dataset.id;
+        var div = document.createElement('div');
+        div.setAttribute('data-id', videoId);
+        var thumbNode = document.createElement('img');
+        thumbNode.src = '../../images/yt-thumb/2.jpg'.replace('ID', videoId);
+        div.appendChild(thumbNode);
+        var playButton = document.createElement('div');
+        playButton.setAttribute('class', 'play');
+        div.appendChild(playButton);
+        div.onclick = function () {
+          labnolIframe(this);
+        };
+        playerElements[n].appendChild(div);
+      }
+    }
+  
+    document.addEventListener('DOMContentLoaded', initYouTubeVideos);
+  </script>
 @endsection

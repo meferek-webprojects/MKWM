37e0dc0261d7735beb32c22c15a2ee1cd9d9d540
Mateusz Krysiak <monti02022@gmail.com>
2022-05-16 12:01:43 +0200

    [UPDATE] Photoshoots look

--- resources/views/main/layouts/dark-layout.blade.php
+++ resources/views/main/layouts/dark-layout.blade.php
@@ -21,34 +21,35 @@
     
     <div class="container-fluid m-0 p-0">
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-xl navbar-dark full">
             <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <i data-feather="menu"></i>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul class="navbar-nav mb-2 mb-lg-0 mx-auto d-flex align-items-center position-relative">
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">Home</a>
+                            <a class="nav-link" aria-current="page" href="#">Główna</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">We</a>
+                            <a class="nav-link" aria-current="page" href="#">My</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">Photography</a>
+                            <a class="nav-link" aria-current="page" href="#">Fotografia</a>
+                        </li>
+                        <li class="nav-item d-none d-xl-block" id="nav-logo">
+                            <a class="nav-link logo" aria-current="page" href="#">MKWM</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">MKWM</a>
+                            <a class="nav-link" aria-current="page" href="#">Filmografia</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">Videography</a>
+                            <a class="nav-link" aria-current="page" href="#">Kontakt</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">Contact</a>
-                        </li>
-                        <li class="nav-item" id="search-button">
+                        <li class="nav-item d-none d-xl-block position-relative" id="search-button">
                             <i data-feather="search"></i>
                         </li>
+                        <input type="text" class="searchbar position-absolute border-0" placeholder="Szukaj sesji">
                     </ul>
                 </div>
             </div>
@@ -59,6 +60,55 @@
     </div>
 
     @yield('added-js')
+
+    <script>
+        $('body').on('click', () => {
+            if($('.searchbar').hasClass('active')){
+                $('.nav-link').toggleClass('hidden');
+                $('.searchbar').toggleClass('active');
+                $('#search-button').toggleClass('active');
+            }
+        });
+
+        $('#search-button').on('click', event => {
+            event.stopPropagation();
+            $('.nav-link').toggleClass('hidden');
+            $('.searchbar').toggleClass('active');
+            $('#search-button').toggleClass('active');
+        });
+
+        $('.searchbar').on('click', event => event.stopPropagation());
+
+        $(document).scroll(() => {
+            let scrollTop = $(document).scrollTop();
+            let filterBlur = (-window.innerHeight / 2 + scrollTop) / 100;
+
+            if(scrollTop){
+                if(window.innerWidth > 8699)
+                    $('.navbar').css({'background-color': '#000a', 'padding-top': '7px'});
+                else
+                    $('.navbar').removeAttr('style');
+
+                if(scrollTop < window.innerHeight)
+                    $('.hero').css({'top': (scrollTop / 2), 'filter': (filterBlur > 0 ? 'blur('+filterBlur+'px)' : '')});
+            }else{
+                $('.navbar').removeAttr('style');
+                $('.hero').removeAttr('style');
+            }
+        })
+
+        $(window).resize(() => {
+            let scrollTop = $(document).scrollTop();
+
+            if(scrollTop){
+                if(window.innerWidth > 1199)
+                    $('.navbar').css({'background-color': '#000a', 'padding-top': '7px'});
+                else
+                    $('.navbar').removeAttr('style');
+            }else
+                $('.navbar').removeAttr('style');
+        })
+    </script>
 
     <script>
         feather.replace();

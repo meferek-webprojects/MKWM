7fce585399fefe21f3831cf6f30c98b71c0f41e7
Mateusz Krysiak <monti02022@gmail.com>
2022-06-16 12:27:37 +0200

    [UPDATE] Type Header

--- resources/views/main/plener.blade.php
+++ resources/views/main/plener.blade.php
@@ -19,7 +19,14 @@
             </div>
         </div>
         <div class="background">
-            <img class="img-fluid" src="{{ url('images/img/natalia.jpg')}}" alt="">
+            @php
+                $header = DB::table('portfolios')->where('kind', 'photo')->where('type', 'plener')->where('type_header', true)->first();
+            @endphp
+            @if(isset($header))
+                <img class="img-fluid" src="{{ url('images/portfolios/'.$header->file) }}" alt="" @if($centered = $header->centered) image-center="{{ $centered }}" @endif>
+            @else
+                <img class="img-fluid" src="{{ url('images/img/natalia.jpg') }}" image-center="0.485 0.5">
+            @endif
         </div>
     </div>
 
@@ -28,7 +35,7 @@
             <div class="row">
                 <div class="col-12" id="lightgallery">
                     @php
-                        $photos = DB::table('portfolios')->join('portfolio_files', 'portfolios.id', '=', 'portfolio_files.portfolio_id')->select('portfolio_files.*', 'portfolios.type')->where('kind', 'photo');  
+                        $photos = DB::table('portfolios')->where('kind', 'photo');
                     @endphp
                     @forelse($photos->where('type', 'plener')->get() as $photo)
                         <a href="{{ url('images/portfolios/'.$photo->file) }}">

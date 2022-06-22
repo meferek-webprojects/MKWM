ca0761fd23deb4e5824d0fcfa504e63db91f3ae5
Mateusz Krysiak <monti02022@gmail.com>
2022-06-22 22:07:22 +0200

    [UPDATE] Session

--- resources/views/dashboard/session.blade.php
+++ resources/views/dashboard/session.blade.php
@@ -194,7 +194,7 @@
                                 <div class="row">
                                     <div class="col-3">
                                         <div class="avatar avatar-rounded">
-                                            @if($aU->avatar !== NULL)
+                                            @if(Auth::user()->avatar !== NULL)
                                                 <img src="{{ url($aU->avatar) }}" alt="" @if($centered = $aU->centered) image-center="{{ $centered }}" @endif>
                                             @else
                                                 <div class="avatar-title">{{ $aU->initials }}</div>
